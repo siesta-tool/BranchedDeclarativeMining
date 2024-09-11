fa43890f6d9ade5f072adeaea9266a1e1bcf0af4
John Mavroudopoulos <mavroudo@csd.auth.gr>
2024-09-11 19:48:38 +0300

    Modified script to run directly in cluster

--- evaluation/incremental_evaluation/script.sh
+++ evaluation/incremental_evaluation/script.sh
@@ -2,13 +2,13 @@
 
 DIRECTORY=/app/output
 PREFIX=$1
-SPARK_SUBMIT_COMMAND="/opt/spark/bin/spark-submit --master local[*]"
+SPARK_SUBMIT_COMMAND="/opt/spark/bin/spark-submit --master local[*] --driver-memory 50g"
 PREPROCESS_JAR="/app/preprocess.jar"
 EXTRACTION_JAR="/app/declare.jar"
 
 export s3accessKeyAws=minioadmin
 export s3ConnectionTimeout=600000
-export s3endPointLoc=http://localhost:9000
+export s3endPointLoc=http://minio:9000
 export s3secretKeyAws=minioadmin
 
 # Check if the directory exists
@@ -20,7 +20,7 @@ if [ -d "$DIRECTORY" ]; then
     echo "Processing $FILENAME"
 
      # Run Spark job using spark-submit and capture the output
-      output=$($SPARK_SUBMIT_COMMAND "$PREPROCESS_JAR" -f "$file" --logname "$PREFIX" 2>&1)
+      output=$($SPARK_SUBMIT_COMMAND "$PREPROCESS_JAR" -f "$file" --logname "$PREFIX" --last_checked_split day -s 3 2>&1)
 
       # Check if the Spark job failed
       if [ $? -ne 0 ]; then
